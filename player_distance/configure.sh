817f02db8a8938eac79c56ffe3a1efbbed4e060f
intellild <intellild@outlook.com>
2021-11-26 17:19:17 +0800

    enable optimize

--- player_distance/configure.sh
+++ player_distance/configure.sh
@@ -2,5 +2,5 @@
 
 mkdir build
 pushd build
-python3 ../configure.py --hl2sdk-root ../../ --mms-path ../../metamod-source --sm-path ../../sourcemod -s csgo
+python3 ../configure.py --hl2sdk-root ../../ --mms-path ../../metamod-source --sm-path ../../sourcemod -s csgo --enable-optimize
 popd
